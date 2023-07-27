d8adcf99840959f58a15afdfeabfa9ce8ab99142
Muhammed <faheem@metrictreelabs.com>
2023-07-27 18:01:24 +0530

    user/model,register api created

---
+++ src/controllers/authController.js
@@ -0,0 +1,70 @@
+import User from "../model/auth/user.model.js";
+import Jwt from "jsonwebtoken";
+import { encryptPassword, checkPassword } from "../services/MiscServices.js";
+
+export const login = async (req, res) => {
+  const {
+    phone,
+    password,
+    email,
+    isEmployer,
+    first_name,
+    last_name,
+    company_address,
+  } = req.body;
+  const encryptedPassword = await encryptPassword(password);
+  try {
+    const existingUser = await User.findOne({ phone: phone });
+    if (existingUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User already registered" });
+    } else {
+      if (req.file == undefined) {
+        let saveuser = new User({
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          password: encryptedPassword,
+          isEmployer: isEmployer,
+          phone: phone,
+          image: null,
+          company_address: company_address,
+        });
+        const saveUser = await saveuser.save();
+        await res.status(201).json({
+          status: 201,
+          success: true,
+          message: "User registred successfully",
+          data: saveUser,
+        });
+      } else {
+        const imagePath = req.file.path;
+        const imageRelativePath = "/" + imagePath.replace(/\\/g, "/");
+        const imageUrl =
+          req.protocol + "://" + req.get("host") + imageRelativePath;
+        const image = imageUrl;
+        let saveuser = new User({
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          password: encryptedPassword,
+          isEmployer: isEmployer,
+          phone: phone,
+          image: image,
+          company_address: company_address,
+        });
+
+        const saveUser = await saveuser.save();
+        await res.status(201).json({
+          status: 201,
+          success: true,
+          message: "User registred successfully",
+          data: saveUser,
+        });
+      }
+    }
+  } catch (error) {
+    await res.status(500).json({ message: error });
+  }
+};
---
+++ src/model/auth/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const CompanyAddressSchema = new mongoose.Schema({
+  company_name: String,
+  state: String,
+  city: String,
+  company_type: String,
+  phone: String,
+  employee_size: Number,
+});
+
+const userModel = new mongoose.Schema(
+  {
+    first_name: { type: String, required: false },
+    last_name: { type: String, required: false },
+    email: { type: String, required: true, unique: true },
+    image: { type: String, required: false },
+    phone: { type: String, required: true, unique: true },
+    isEmployer: { type: Boolean, required: false },
+    password: { type: String, required: true },
+    access_token: { type: String, default: "" },
+    refresh_token: { type: String, default: "" },
+    company_address: [
+      {
+        type: CompanyAddressSchema,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userModel);
+export default User;
