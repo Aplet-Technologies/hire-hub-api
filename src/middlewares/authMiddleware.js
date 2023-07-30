7bfd8f69024cd9e9e7f91ee2213a09a6acedfac6
Muhammed <faheem@metrictreelabs.com>
2023-07-30 17:18:56 +0530

    minddleware added

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,38 @@
+import Jwt from "jsonwebtoken";
+import User from "../model/auth/user.model.js";
+
+export const accessToken = async (req, res, next) => {
+  // let myUser = await req.session.user;
+  //   let device = await req.headers["device"];
+  let authHeader = await req.headers["authorization"];
+  let token = authHeader && authHeader.split(" ")[1]; //Access token
+  var filter;
+  await Jwt.verify(token, "access-key-secrete", (err, access) => {
+    filter = { _id: access?.user_data?.user_id };
+
+    User.find(filter).then((result) => {
+      const user = result[0];
+      if (!user) {
+        return res
+          .status(403)
+          .json({ status: "error", message: "Unauthenticated" });
+      }
+      //   if (user) {
+      //     if (device !== user.device) {
+      //       return res
+      //         .status(403)
+      //         .json({ status: "error", message: "session expired" });
+      //     }
+      //   }
+
+      if (!err) {
+        req.user = access;
+        next();
+      } else {
+        return res
+          .status(403)
+          .json({ status: "error", message: "Unauthenticated" });
+      }
+    });
+  });
+};
