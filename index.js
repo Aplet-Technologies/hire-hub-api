a21710e8b94f1151c40313197af29fb0d4fb991b
Muhammed <faheem@metrictreelabs.com>
2023-11-14 11:58:43 +0530

    updates

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import path from "path";
 import router from "./src/routes/index.js";
+import { createServer } from "http";
 // db
 import { mongo } from "./src/config/db.js";
 
@@ -12,15 +13,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+const server = createServer(app); // Create an HTTP server
 app.use("/api", router);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// const dbUrl = `mongodb+srv://faheemtfora:sydesNQFyzlnOIOK@cluster0.vlmalrp.mongodb.net/?retryWrites=true&w=majority`;
-const dbUrl = `mongodb://0.0.0.0:27017/hirehubdb`;
+const dbUrl = `mongodb+srv://faheemtfora:sydesNQFyzlnOIOK@cluster0.vlmalrp.mongodb.net/?retryWrites=true&w=majority`;
+// const dbUrl = `mongodb://0.0.0.0:27017/hirehubdb`;
 
 mongo(dbUrl);
 
-app.listen(8000, () => {
+server.listen(8000, () => {
   console.log(`Server started on port 8000,connected DB ${dbUrl}`);
 });
 
